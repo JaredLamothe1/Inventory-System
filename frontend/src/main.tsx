0d3db57ff6a7b863238c509a29b58723c3b1f0ad
Jared <jaredlamothe2@gmail.com>
2025-06-14 01:14:17 -0400

    Fixed main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
